e62ed85ba9d69ebb81d64bf6e6305dbbb5679e77
j_pearl <y2kwlsw@gmail.com>
2023-01-06 16:13:42 +0900

    test: 구글 드라이브 연동 테스트

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import geocoder
+import pandas as pd
+import pickle
+# g = geocoder.ip('me')
+
+# g  = g.latlng
+# latitude = g[0]
+# longitude = g[1]
+# st.write(f"{latitude}:{longitude}")
+
+
+# streamlit_app.py
+
+from google.oauth2 import service_account
+from google.cloud import storage
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
+
+bucket_name = "streamlit-bucket-nyamnyam"
+file_path = "all_data.pkl"
+
+content = read_file(bucket_name, file_path)
+
+print(type(content))
+
+# # load
+# with open('data.pickle', 'rb') as f:
+#     data = pickle.load(f)
